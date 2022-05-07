f329cc8ddeeb0c781ccea15ca9deacb9632dc96b
DimalMaloku1 <dm51522@ubt-uni.net>
2022-05-07 21:37:20 +0200

    faqja e shportes

---
+++ cart.php
@@ -0,0 +1,106 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+   header('location:user_login.php');
+};
+
+if(isset($_POST['delete'])){
+   $cart_id = $_POST['cart_id'];
+   $delete_cart_item = $conn->prepare("DELETE FROM `cart` WHERE id = ?");
+   $delete_cart_item->execute([$cart_id]);
+}
+
+if(isset($_GET['delete_all'])){
+   $delete_cart_item = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
+   $delete_cart_item->execute([$user_id]);
+   header('location:cart.php');
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>shporta</title>
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="products shopping-cart">
+
+   <h3 class="heading">shporta e blerjeve</h3>
+
+   <div class="box-container">
+
+   <?php
+      $grand_total = 0;
+      $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+      $select_cart->execute([$user_id]);
+      if($select_cart->rowCount() > 0){
+         while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){
+   ?>
+   <form action="" method="post" class="box">
+      <input type="hidden" name="cart_id" value="<?= $fetch_cart['id']; ?>">
+      <img src="uploaded_img/<?= $fetch_cart['image']; ?>" alt="">
+      <div class="name"><?= $fetch_cart['name']; ?></div>
+      <div class="flex">
+      </div>
+      <div class="sub-total"> Qmimi : <span><?= $sub_total = ($fetch_cart['price'] ); ?>€</span> </div>
+      <input type="submit" value="fshije" onclick="return confirm('fshije kursin nga shporta?');" class="delete-btn" name="delete">
+   </form>
+   <?php
+   $grand_total += $sub_total;
+      }
+   }else{
+      echo '<p class="empty">shporta eshte e zbrazet</p>';
+   }
+   ?>
+   </div>
+
+   <div class="cart-total">
+      <p>Krejt Totali : <span><?= $grand_total; ?>€</span></p>
+      <a href="shop.php" class="option-btn">kaloni ne dyqan</a>
+      <a href="cart.php?delete_all" class="delete-btn <?= ($grand_total > 1)?'':'disabled'; ?>" onclick="return confirm('delete all from cart?');">fshiji te gjitha</a>
+      <a href="checkout.php" class="btn <?= ($grand_total > 1)?'':'disabled'; ?>">vazhdoni porosin</a>
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
