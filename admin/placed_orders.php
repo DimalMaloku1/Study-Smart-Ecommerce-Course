7a4b08585014770f517cc52eb84aa87a697eb871
DimalMaloku1 <96485417+DimalMaloku1@users.noreply.github.com>
2022-05-08 15:33:23 +0200

    faqja e pagesave te userave

--- admin/placed_orders.php
+++ admin/placed_orders.php
@@ -34,7 +34,7 @@ if(isset($_GET['delete'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>porosia</title>
+   <title>pagesat</title>
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
@@ -47,7 +47,7 @@ if(isset($_GET['delete'])){
 
 <section class="orders">
 
-<h1 class="heading">vendi i porosise</h1>
+<h1 class="heading">menyra e pageses</h1>
 
 <div class="box-container">
 
